2b2d419be503ab1ae1922e71e6f600eaba13bb11
Alvaro Sanchez-Mariscal <alvaro.sanchez@odobo.com>
2013-12-17 13:31:35 +0100

    Cleanup, dependencies added

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,12 +8,15 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenLocal()
         mavenCentral()
+        mavenRepo 'http://repo.spring.io/milestone' // TODO remove
     }
 
     dependencies {
     }
 
     plugins {
+        compile ':spring-security-core:2.0-RC2'
+
         build(":release:3.0.1", ":rest-client-builder:1.0.3") {
             export = false
         }
