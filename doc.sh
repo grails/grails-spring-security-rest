2ece12fb5de8e0204c5bf6f408f8b89278b0cc1a
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2013-12-30 16:48:27 +0100

    Documentation updated

--- doc.sh
+++ doc.sh
@@ -3,10 +3,12 @@
 grails doc
 rm -rf /tmp/docs/
 mv target/docs /tmp
+
 git checkout gh-pages
 rm -rf docs/
-git mv /tmp/docs .
+mv /tmp/docs .
 git add -A
 git commit -m "Documentation updated"
 git push origin gh-pages
+
 git checkout master
