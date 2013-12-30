5447c48d39e4d58fa583f7da77b650726dd87cbd
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2013-12-30 18:06:03 +0100

    Script updated

--- doc.sh
+++ doc.sh
@@ -8,6 +8,9 @@ rm -rf /tmp/docs/
 mv target/docs /tmp
 
 git checkout gh-pages
+cp index.tmpl index.html
+sed -i -e "s/{{VERSION}}/${version}/g" index.html
+
 rm -rf docs/
 mv /tmp/docs .
 git add -A
