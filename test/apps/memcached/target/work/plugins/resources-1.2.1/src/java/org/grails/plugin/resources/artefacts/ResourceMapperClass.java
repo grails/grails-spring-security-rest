bc70f45b62412a4867ae6fde775606bddf1587f6
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2013-12-30 16:26:12 +0100

    Restart

--- test/apps/memcached/target/work/plugins/resources-1.2.1/src/java/org/grails/plugin/resources/artefacts/ResourceMapperClass.java
+++
@@ -1,8 +0,0 @@
-package org.grails.plugin.resources.artefacts;
-
-import org.codehaus.groovy.grails.commons.GrailsClass;
-
-/**
- * @author Luke Daley (ld@ldaley.com)
- */
-public interface ResourceMapperClass extends GrailsClass {}
