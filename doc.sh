2667afbf5cdfa6c767adf7160041d06d69cf1b72
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2013-12-30 16:42:23 +0100

    Updated doc script

--- doc.sh
+++ doc.sh
@@ -6,7 +6,7 @@ mv target/docs /tmp
 git checkout gh-pages
 rm -rf docs/
 git mv /tmp/docs .
-git add .
-git commit -m "Documentation updated" .
+git add -A
+git commit -m "Documentation updated"
 git push origin gh-pages
 git checkout master
