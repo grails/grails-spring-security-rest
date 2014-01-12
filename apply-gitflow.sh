dccaae176715381056351deda319c924fd31ab04
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2014-01-12 03:36:09 +0100

    typo

--- apply-gitflow.sh
+++ apply-gitflow.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 git config gitflow.branch.master 'master'
 git config gitflow.branch.develop 'develop'
 git config gitflow.prefix.feature 'feature/'
@@ -6,5 +7,4 @@ git config gitflow.prefix.release 'release/'
 git config gitflow.prefix.hotfix 'hotfix/'
 git config gitflow.prefix.support 'support/'
 git config gitflow.prefix.versiontag ''
-git config gitflow.feature.finish.rebase true
 git config gitflow.feature.finish.rebase true
