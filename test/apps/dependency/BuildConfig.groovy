92a6ff356e61458255bf39677b8ff87d14eef4bf
Alvaro Sanchez-Mariscal <alvaro.sanchez@odobo.com>
2014-01-31 10:30:46 +0100

    Release 1.2.2

--- test/apps/dependency/BuildConfig.groovy
+++ test/apps/dependency/BuildConfig.groovy
@@ -63,7 +63,7 @@ grails.project.dependency.resolution = {
         compile ':cache:1.1.1'
 
         compile ':spring-security-core:2.0-RC2'
-        compile ":spring-security-rest:1.2.2-SNAPSHOT", {
+        compile ":spring-security-rest:1.2.2", {
             excludes: 'spring-security-core'
         }
 
