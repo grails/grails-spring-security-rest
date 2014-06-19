672c226280e81e7f3ab88258e0443d39ee4dfb9f
Alvaro Sanchez-Mariscal <alvaro.sanchez@odobo.com>
2014-06-19 16:50:48 +0200

    Fix for #75

--- test/apps/memcached/test/functional/com/odobo/grails/plugin/springsecurity/rest/BearerTokenSpec.groovy
+++ test/apps/memcached/test/functional/com/odobo/grails/plugin/springsecurity/rest/BearerTokenSpec.groovy
@@ -4,10 +4,27 @@ import grails.plugins.rest.client.ErrorResponse
 import grails.plugins.rest.client.RestResponse
 import grails.util.Holders
 import spock.lang.IgnoreIf
+import spock.lang.Issue
 
 @IgnoreIf({ !Holders.config.grails.plugin.springsecurity.rest.token.validation.useBearerToken })
 class BearerTokenSpec extends AbstractRestSpec {
 
+    @Issue("https://github.com/alvarosanchez/grails-spring-security-rest/issues/75")
+    void "access token response is compliant with the specification"() {
+        when:
+        RestResponse response = sendCorrectCredentials()
+
+        then:
+        response.status == 200
+        response.headers.getFirst('Content-Type') == 'application/json;charset=UTF-8'
+        response.headers.getFirst('Cache-Control') == 'no-store'
+        response.headers.getFirst('Pragma') == 'no-cache'
+        response.json.access_token
+        response.json.token_type == 'Bearer'
+
+    }
+
+    @Issue("https://github.com/alvarosanchez/grails-spring-security-rest/issues/73")
     void "authorisation header is checked to read token value"() {
         given:
         RestResponse authResponse = sendCorrectCredentials()
@@ -22,6 +39,7 @@ class BearerTokenSpec extends AbstractRestSpec {
         response.status == 200
     }
 
+    @Issue("https://github.com/alvarosanchez/grails-spring-security-rest/issues/73")
     void "if credentials are required but missing, the response contains WWW-Authenticate header"() {
         when:
         ErrorResponse response = restBuilder.get("${baseUrl}/secured")
@@ -31,6 +49,7 @@ class BearerTokenSpec extends AbstractRestSpec {
         response.responseHeaders.getFirst('WWW-Authenticate') == 'Bearer'
     }
 
+    @Issue("https://github.com/alvarosanchez/grails-spring-security-rest/issues/73")
     void "if the token is invalid, it is indicated in the header"() {
         when:
         ErrorResponse response = restBuilder.get("${baseUrl}/secured") {
