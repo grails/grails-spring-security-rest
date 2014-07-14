d613c45bec8faae4cada7290a5925f7acff663e8
Alvaro Sanchez-Mariscal <alvaro.sanchez@odobo.com>
2014-07-14 10:55:20 +0200

    1.4.0.RC5

--- SpringSecurityRestGrailsPlugin.groovy
+++ SpringSecurityRestGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import javax.servlet.http.HttpServletResponse
 
 class SpringSecurityRestGrailsPlugin {
 
-    String version = "1.4.0.RC4"
+    String version = "1.4.0.RC5"
     String grailsVersion = "2.0 > *"
     List loadAfter = ['springSecurityCore']
     List pluginExcludes = [
