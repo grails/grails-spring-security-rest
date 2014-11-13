72b87c3ba5f9846c2ea563572eb3b49b8f654b8f
aeischeid <a.eischeid@gmail.com>
2014-11-12 18:02:02 -0600

    not quite working attempt at bootstrapping some users and roles

--- test/apps/gormTest/grails-app/conf/BootStrap.groovy
+++ test/apps/gormTest/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,23 @@
+import com.AppUser
+import com.Role
+import com.AppUserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        def userRole = new Role(authority:'ROLE_USER')
+        def adminRole = new Role(authority:'ROLE_ADMIN')
+
+        userRole.save()
+        adminRole.save()
+
+        def jimi = new AppUser(username:'jimi', password:'jimispassword')
+        def alvaro = new AppUser(username:'115537660854424164575', password:'N/A')
+
+        AppUserRole.create jimi, userRole, true
+        AppUserRole.create alvaro, userRole, true
+        AppUserRole.create alvaro, adminRole, true
+
     }
     def destroy = {
     }
