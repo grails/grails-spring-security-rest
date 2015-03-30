c6f0213137764241dd167d44994cdd2f86d96322
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-03-30 14:34:59 +0200

    Publishing test results to Divshot

--- publish-test-results.sh
+++ publish-test-results.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Creating folder target/divshot/$TRAVIS_JOB_NUMBER"
 mkdir -p target/divshot/$TRAVIS_JOB_NUMBER
-cd target/divshot
-divshot -t $DIVSHOT_TOKEN config:add name spring-security-rest
-divshot -t $DIVSHOT_TOKEN pull production
-rm -rf *
-cd ../..
 mv target/test-reports target/divshot/$TRAVIS_JOB_NUMBER/core
 for app in `ls test/apps` ; do mv test/apps/$app/target/test-reports target/divshot/$TRAVIS_JOB_NUMBER/$app ; done
 cd target/divshot
 divshot -t $DIVSHOT_TOKEN config:add name spring-security-rest
+divshot -t $DIVSHOT_TOKEN pull production
+divshot -t $DIVSHOT_TOKEN files production
 divshot -t $DIVSHOT_TOKEN push production
+ls -l
+divshot -t $DIVSHOT_TOKEN files production
