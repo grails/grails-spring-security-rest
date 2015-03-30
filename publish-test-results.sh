1697392814f547a7b8cde2f3a180a8ecf4e7e2a7
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-03-30 17:54:30 +0200

    Publishing test results to Divshot

--- publish-test-results.sh
+++ publish-test-results.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Renaming files with spaces"
-./renameFiles.groovy target
 
 echo "Creating folder target/divshot/$TRAVIS_JOB_NUMBER"
 mkdir -p target/divshot/$TRAVIS_JOB_NUMBER
 mv target/test-reports target/divshot/$TRAVIS_JOB_NUMBER/core
 for app in `ls test/apps` ; do mv test/apps/$app/target/test-reports target/divshot/$TRAVIS_JOB_NUMBER/$app ; done
 cd target/divshot
+
+echo "Renaming files with spaces"
+./renameFiles.groovy .
+
 divshot -t $DIVSHOT_TOKEN config:add name spring-security-rest
 divshot -t $DIVSHOT_TOKEN pull production
 divshot -t $DIVSHOT_TOKEN push production
