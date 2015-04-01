aeb0fd6b97f043a244f151ba86c33913db47abba
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-04-01 15:53:54 +0200

    Automating plugin publishing
    [skip ci]

---
+++ publish-plugin.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "grails.project.repos.grailsCentral.username = 'alvaro.sanchez'" >> ~/.grails/settings.groovy
+echo "grails.project.repos.grailsCentral.username = '${PLUGIN_PORTAL_PASSWORD}'" >> ~/.grails/settings.groovy
+
+./grailsw clean
+./grailsw package-plugin
+./grailsw publish-plugin
