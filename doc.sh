dbf383a206bfa15b248769def22b357afc25a849
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-04-01 13:22:21 +0200

    Attempt to automate releases to GitHub

--- doc.sh
+++ doc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
-git config --global credential.helper "store --file=~/.git-credentials"
-echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
-
-./grailsw doc
-
-version=`cat SpringSecurityRestGrailsPlugin.groovy | grep version | sed -e 's/^.*"\(.*\)"$/\1/g'`
-find target/docs/guide -name "*.html" | xargs sed -e "s/&#123;&#123;VERSION&#125;&#125;/${version}/g" -i
-
-
-
 if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
 	# If there is a tag present then this becomes the latest
 	if [[ -n $TRAVIS_TAG ]]; then
+
+		git config --global user.name "$GIT_NAME"
+		git config --global user.email "$GIT_EMAIL"
+		git config --global credential.helper "store --file=~/.git-credentials"
+		echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+		./grailsw doc
+
+		version=`cat SpringSecurityRestGrailsPlugin.groovy | grep version | sed -e 's/^.*"\(.*\)"$/\1/g'`
+		find target/docs/guide -name "*.html" | xargs sed -e "s/&#123;&#123;VERSION&#125;&#125;/${version}/g" -i
+
+
 		git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
 		cd gh-pages
 
