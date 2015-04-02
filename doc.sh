dc0e07adfa2167ffd1b54bd295789b89857a7c63
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-04-02 09:05:45 +0200

    Only one build with Java 8. Fixes to the documentation process.

--- doc.sh
+++ doc.sh
@@ -5,22 +5,24 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
 	# If there is a tag present then this becomes the latest
 	if [[ -n $TRAVIS_TAG ]]; then
-
-		git config --global user.name "$GIT_NAME"
-		git config --global user.email "$GIT_EMAIL"
-		git config --global credential.helper "store --file=~/.git-credentials"
-		echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
-
 		./grailsw doc
 
 		version=`cat SpringSecurityRestGrailsPlugin.groovy | grep version | sed -e 's/^.*"\(.*\)"$/\1/g'`
 		find target/docs/guide -name "*.html" | xargs sed -e "s/&#123;&#123;VERSION&#125;&#125;/${version}/g" -i
 
 		export TAG_LIST=`git tag`
+		echo "List of releases: ${TAG_LIST}"
+		echo "Preparing release of version $version"
 
-		echo "* Preparing release of version $version"
+		echo "Configuring git with name ${GIT_NAME} and email ${GIT_EMAIL}"
 
-		git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
+		git config --global user.name "$GIT_NAME"
+		git config --global user.email "$GIT_EMAIL"
+		git config --global credential.helper "store --file=~/.git-credentials"
+		echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+		git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages > /dev/null
+
 		cd gh-pages
 
 		./gradlew generateIndex
@@ -28,6 +30,7 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
 		mkdir -p "$version"
 		mv ../target/docs "./$version/"
 		git add "$version/*"
+
 
 		git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
 		git push origin HEAD
