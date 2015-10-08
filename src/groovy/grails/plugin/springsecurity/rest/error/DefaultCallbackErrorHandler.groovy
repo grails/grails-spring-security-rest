d1fcccc5c569b2bd3c9c346604a205ceb617237a
Dónal Murtagh <donal.murtagh@cognovi.ie>
2015-10-08 10:20:15 +0100

    #237 added license header

--- src/groovy/grails/plugin/springsecurity/rest/error/DefaultCallbackErrorHandler.groovy
+++ src/groovy/grails/plugin/springsecurity/rest/error/DefaultCallbackErrorHandler.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2015 Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package grails.plugin.springsecurity.rest.error
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException
@@ -8,6 +24,8 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 /**
  * Error handler that's backwardly compatible with the behaviour that was embedded within the callback action of
  * RestOauthController up to and including version 1.5.2
+ *
+ * @author Dónal Murtagh
  */
 class DefaultCallbackErrorHandler implements CallbackErrorHandler {
 
