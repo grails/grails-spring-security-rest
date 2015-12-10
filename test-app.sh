2fc5c89f2c557875a156418a3d32813043c6e286
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-12-10 19:28:16 +0100

    Full build process restored

--- test-app.sh
+++ test-app.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-./gradlew clean install test license
-
-#./gradlew install test \
-#  && cd test/apps \
-#  && for app in `ls .`; do
-#     cd $app && ./test-app.sh && cd ..
-#     if [ $? -ne 0 ]; then
-#        echo -e "\033[0;31mTests FAILED\033[0m"
-#        exit -1
-#     fi
-#     done \
-#  && cd ../../
+./gradlew clean install check license \
+  && cd test/apps \
+  && for app in `ls .`; do
+     cd $app && ./test-app.sh && cd ..
+     if [ $? -ne 0 ]; then
+        echo -e "\033[0;31mTests FAILED\033[0m"
+        exit -1
+     fi
+     done \
+  && cd ../../
--- test/apps/jwt/test-app.sh
+++ test/apps/jwt/test-app.sh
@@ -3,7 +3,7 @@
 rm -f grails-app/conf/application.groovy \
     && for config in `ls grails-app/conf/application*.groovy`; do
         cp $config grails-app/conf/application.groovy \
-           && ./gradlew clean integrationTest \
+           && ./gradlew clean check \
            && rm grails-app/conf/application.groovy
            if [ $? -ne 0 ]; then
                 echo -e "\033[0;31mTests FAILED\033[0m"
