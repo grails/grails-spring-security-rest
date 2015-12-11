563c8171fd63fa06a9b4961a58dea749ec461ba8
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2015-12-11 19:02:03 +0100

    Doc publishing changes

--- publish-plugin.sh
+++ publish-plugin.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-[[ ! -z "$BINTRAY_KEY" ]] && echo "bintrayKey=$BINTRAY_KEY" >> ~/.gradle/gradle.properties
-[[ ! -z "$PLUGIN_PORTAL_PASSWORD" ]] && echo "pluginPortalPassword=$BINTRAY_KEY" >> ~/.gradle/gradle.properties
-
 echo "Publishing plugin"
-./gradlew clean publishPlugin notifyPluginPortal
+./gradlew clean publishPlugin notifyPluginPortal --stacktrace
