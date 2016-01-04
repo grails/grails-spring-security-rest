ddf73848f31a81ca437c4eb1be7efd077241bffa
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2016-01-04 17:38:38 +0100

    Variable list of test apps

--- test-app.sh
+++ test-app.sh
@@ -17,7 +17,7 @@ sdk use grails $grailsVersion
 
 ./gradlew check install \
   && cd build && rm -rf * \
-  && for feature in jwt1 jwt2 memcached1 memcached2 redis1 redis2 grailscache1 grailscache2; do
+  && for feature in `ls ../spring-security-rest-testapp-profile/features/`; do
      grails create-app -profile org.grails.plugins:spring-security-rest-testapp-profile:$pluginVersion -features $feature $feature && cd $feature && ./gradlew check && cd ..
      if [ $? -ne 0 ]; then
        echo -e "\033[0;31mTests FAILED\033[0m"
