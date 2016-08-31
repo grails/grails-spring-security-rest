33f2abcd5b4f7238e7dbe29dbd10a470609f9ec4
Álvaro Sánchez-Mariscal <mariscal@MacBook-Pro-de-Alvaro-2.local>
2016-09-01 01:23:43 +0200

    Not uploading test reports

--- test-app.sh
+++ test-app.sh
@@ -22,8 +22,6 @@ sdk use grails $grailsVersion
      grails create-app -profile org.grails.plugins:spring-security-rest-testapp-profile:$pluginVersion -features $feature $feature && cd $feature && ./gradlew check && cd ..
      if [ $? -ne 0 ]; then
        echo -e "\033[0;31mTests FAILED\033[0m"
-       cd ../..
-       ./upload-test-reports.sh
        exit -1
      fi
      done \
