3b884f768dd4f10b26d2c49670352889b48e322d
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2018-03-04 01:51:09 +0100

    Improvements to the publication process

--- publish-plugin.sh
+++ publish-plugin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Publishing plugin"
-./gradlew clean publishPlugin notifyPluginPortal -x :spring-security-rest-testapp-profile:notifyPluginPortal --stacktrace
+./gradlew clean publishPlugin --stacktrace
